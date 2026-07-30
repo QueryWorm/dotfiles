cfb2d6087419a1373dbb8d5b7bbcd03d125f2277
QueryWorm <222608355+QueryWorm@users.noreply.github.com>
2026-07-30 12:24:08 +0300

    add DEV/venv convention (mkproject, venvon)

--- zsh/hosts/wsl.zsh
+++ zsh/hosts/wsl.zsh
@@ -1,2 +1,2 @@
-alias venv='source ~/DEV/venv/bin/activate'
+alias venv-legacy='source ~/DEV/venv/bin/activate'
 alias naweb='cd ~/DEV/NakedWeb && uvicorn main:app --host 0.0.0.0 --port 3842 --reload'
